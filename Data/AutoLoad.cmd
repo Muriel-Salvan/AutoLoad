798cf9f349e84baf075c59549a31a800b4d0d6a2
Muriel Salvan <muriel@x-aeon.com>
2021-01-19 14:19:07 +0100

    Set default values

--- Data/AutoLoad.cmd
+++ Data/AutoLoad.cmd
@@ -1,7 +1,15 @@
 @echo off
-set gameDir=C:/Program Files (x86)/Steam/steamapps/common/Skyrim Special Edition
-set gameExe=skse64_loader.exe
-set autoHotkeyCmd=C:\Program Files\AutoHotkey\AutoHotkey.exe
+
+REM Set default values
+IF NOT DEFINED gameDir (
+  set "gameDir=C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition"
+)
+IF NOT DEFINED gameExe (
+  set gameExe=skse64_loader.exe
+)
+IF NOT DEFINED autoHotkeyCmd (
+  set "autoHotkeyCmd=C:\Program Files\AutoHotkey\AutoHotkey.exe"
+)
 
 REM Run AHK in parallel, waiting for the game to load the saved game
 START /B "%autoHotkeyCmd%" "%gameDir%\Data\AHK\AutoLoad.ahk" %1
